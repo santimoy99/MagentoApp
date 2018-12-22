406e496d5a2587fd0878a9046170faecc665da6f
santimoy <santimoy@DESKTOP-F6N6R57>
2018-12-22 15:09:19 +0530

    Secand

---
+++ TestNGSuite1/src/Com/abc/Demo/Excludemethod.java
@@ -0,0 +1,27 @@
+package Com.abc.Demo;
+
+import org.testng.annotations.Test;
+
+public class Excludemethod {
+
+	@Test
+	public void Test1() {
+		System.out.println("Test1 is ready for execution");
+	}
+	@Test
+	public void Test2() {
+		System.out.println("Test2 is ready for execution");
+	}
+	@Test
+	public void Test3() {
+		System.out.println("Test3 is ready for execution");
+	}
+	@Test
+	public void Test4() {
+		System.out.println("Test4 is ready for execution");
+	}
+	@Test
+	public void Test5() {
+		System.out.println("Test5 is ready for execution");
+	}
+}
